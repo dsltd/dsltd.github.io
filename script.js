667ef27b7a942af3235bdf5504b0861b18748dde
Dolphin Studios LTD <dolphinstudiosltd@gmail.com>
2022-10-11 23:16:28 +0100

    VERSION I LOVE YOU MADINA

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function close_options(){
+  var x = document.getElementById('options_popup');
+  x.style.display = "none";
+}
+
+function open_options(){
+  var x = document.getElementById('options_popup');
+  x.style.display = "block";
+}
+
+function open_link(x){
+  window.open(x,'_self');
+}
